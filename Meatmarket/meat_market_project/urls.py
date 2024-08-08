c76694a950ab20b4a6400bc50caaf53b86eede49
Gobinath M <130783700+Go7bi@users.noreply.github.com>
2024-08-08 19:26:49 +0530

    Update urls.py

--- Meatmarket/meat_market_project/urls.py
+++ Meatmarket/meat_market_project/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('market.url')),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
