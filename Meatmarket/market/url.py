cb44829a162d14e9a3cb0ff1f200b3f27c324b0a
Gobinath M <130783700+Go7bi@users.noreply.github.com>
2024-08-08 19:26:03 +0530

    Update url.py

--- Meatmarket/market/url.py
+++ Meatmarket/market/url.py
@@ -2,5 +2,8 @@ from django.urls import path
 from market import views
 urlpatterns = [
     path('home',views.home,name="home"),
-    path('register',views.home,name="register")
+    path('register',views.register,name="register"),
+    path('category',views.category,name="category"),
+    path('category/<str:name>',views.collectionsview,name="category"),
+    path('category/<str:cname>/<str:pname>',views.product_details,name="product_details"),
 ]
