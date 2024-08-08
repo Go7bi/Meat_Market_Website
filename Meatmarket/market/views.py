37f6cc91a51921238427b2e77a60c28372027f04
Gobinath M <130783700+Go7bi@users.noreply.github.com>
2024-08-08 11:52:53 +0530

    Update views.py

--- Meatmarket/market/views.py
+++ Meatmarket/market/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from . models import *
+from django.contrib import messages
 
 def home(request):
     return render(request,"market/index.html")
-def home(request):
+def register(request):
     return render(request,"market/register.html")
+def category(request):
+    catagory=Catagory.objects.filter(status=0)
+    return render(request,"market/category.html",{"catagory":catagory})
+
+def collectionsview(request,name):
+    if(Catagory.objects.filter(name=name,status=0)):
+        products=Product.objects.filter(category__name=name)
+        return render(request,"market/products/index.html",{"products":products})
+    else:
+        messages.warning(request,"No Such Catagory Found")
+        return redirect('category')
+
