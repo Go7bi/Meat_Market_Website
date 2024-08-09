4977b0de37200695b89e52d844aefff0a233d014
Gobinath M <130783700+Go7bi@users.noreply.github.com>
2024-08-09 19:16:22 +0530

    Update views.py

--- Meatmarket/market/views.py
+++ Meatmarket/market/views.py
@@ -1,11 +1,77 @@
-from django.shortcuts import render
+from django.http import  JsonResponse
+from django.shortcuts import redirect, render
+from market.form import CustomUserForm
 from . models import *
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+import json
 
 def home(request):
-    return render(request,"market/index.html")
+    products = Product.objects.filter(trending = 1)
+    return render(request,"market/index.html",{'products':products})
+
+def logout_page(request):
+  if request.user.is_authenticated:
+    logout(request)
+    messages.success(request,"Logged out Successfully")
+  return redirect("/")
+
+def add_to_cart(request):
+   if request.headers.get('x-requested-with')=='XMLHttpRequest':
+    if request.user.is_authenticated:
+      data=json.load(request)
+      product_qty=data['product_qty']
+      product_id=data['pid']
+      #print(request.user.id)
+      product_status=Product.objects.get(id=product_id)
+      if product_status:
+        if Cart.objects.filter(user=request.user.id,product_id=product_id):
+          return JsonResponse({'status':'Product Already in Cart'}, status=200)
+        else:
+          if product_status.quantity>=product_qty:
+            Cart.objects.create(user=request.user,product_id=product_id,product_qty=product_qty)
+            return JsonResponse({'status':'Product Added to Cart'}, status=200)
+          else:
+            return JsonResponse({'status':'Product Stock Not Available'}, status=200)
+    else:
+      return JsonResponse({'status':'Login to Add Cart'}, status=200)
+   else:
+    return JsonResponse({'status':'Invalid Access'}, status=200)
+   
+def login_page(request):
+  if request.user.is_authenticated:
+    return redirect("/")
+  else:
+    if request.method=='POST':
+      name=request.POST.get('username')
+      pwd=request.POST.get('password')
+      user=authenticate(request,username=name,password=pwd)
+      if user is not None:
+        login(request,user)
+        messages.success(request,"Logged in Successfully")
+        return redirect("/")
+      else:
+        messages.error(request,"Invalid User Name or Password")
+        return redirect("/login")
+    return render(request,"market/login.html")
+
+def cart_page(request):
+  if request.user.is_authenticated:
+    cart=Cart.objects.filter(user=request.user)
+    return render(request,"market/cart.html",{"cart":cart})
+  else:
+    return redirect("/")
+  
 def register(request):
-    return render(request,"market/register.html")
+  form=CustomUserForm()
+  if request.method=='POST':
+    form=CustomUserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request,"Registration Success You can Login Now..!")
+      return redirect('/login')
+  return render(request,"market/register.html",{'form':form})
+
 def category(request):
     catagory=Catagory.objects.filter(status=0)
     return render(request,"market/category.html",{"catagory":catagory})
@@ -13,7 +79,7 @@ def category(request):
 def collectionsview(request,name):
     if(Catagory.objects.filter(name=name,status=0)):
         products=Product.objects.filter(category__name=name)
-        return render(request,"market/products/index.html",{"products":products})
+        return render(request,"market/products/index.html",{"products":products,"category_name":name})
     else:
         messages.warning(request,"No Such Catagory Found")
         return redirect('category')
