6dd3fcd0253969d79757c3915baedc32b5d9cae1
Gobinath M <130783700+Go7bi@users.noreply.github.com>
2024-08-09 19:14:29 +0530

    Update models.py

--- Meatmarket/market/models.py
+++ Meatmarket/market/models.py
@@ -30,6 +30,15 @@ class Product(models.Model):
   description=models.TextField(max_length=500,null=False,blank=False)
   status=models.BooleanField(default=False,help_text="0-show,1-Hidden")
   trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+
+   def __str__(self) :
+    return self.name
+  
+class Cart(models.Model):
+  user=models.ForeignKey(User,on_delete=models.CASCADE)
+  product=models.ForeignKey(Product,on_delete=models.CASCADE)
+  product_qty=models.IntegerField(null=False,blank=False)
+  created_at=models.DateTimeField(auto_now_add=True)
   created_at=models.DateTimeField(auto_now_add=True)
  
   def __str__(self) :
